234321f032e6ecd06173574f6e2f66e678a85d9c
yarungrung <s1243032@gm.ncue.edu.tw>
2025-10-22 02:01:13 +0000

    modified:   app.py

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 import streamlit as st
- st.title("my first Streamlit App")
- st.header("Hello, Geographers!(From Codespaces!)")
+st.title("my first Streamlit App")
+st.header("Hello, Geographers!(From Codespaces!)")
