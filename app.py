44ea5b2cf93b8202c1074b3c63c49d76b688a06a
yarungrung <s1243032@gm.ncue.edu.tw>
2025-10-22 02:27:58 +0000

    modified:   app.py

--- app.py
+++ app.py
@@ -25,10 +25,10 @@ if st.button("點我顯示氣球!"):
 st.balloons() 
 
 # 檔案上傳 (File Uploader) - 地理系必備!
- uploaded_file = st.file_uploader(
- "上傳你的Shapefile (.zip) 或GeoTIFF (.tif) 或GeoJSON (.json)",
- type=["zip", "tif", "json"]
- )
+uploaded_file = st.file_uploader(
+"上傳你的Shapefile (.zip) 或GeoTIFF (.tif) 或GeoJSON (.json)",
+type=["zip", "tif", "json"]
+)
 
- if uploaded_file is not None:
- st.success(f"你上傳了: {uploaded_file.name} (大小 : {uploaded_file.size} bytes)")
+if uploaded_file is not None:
+st.success(f"你上傳了: {uploaded_file.name} (大小 : {uploaded_file.size} bytes)")
