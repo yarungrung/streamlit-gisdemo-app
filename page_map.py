7210884d204d26bdb56d82bb7d2edef33afcedf0
yarungrung <s1243032@gm.ncue.edu.tw>
2025-10-26 21:23:36 +0000

    modified:   page_map.py

--- page_map.py
+++ page_map.py
@@ -18,7 +18,7 @@ with st.sidebar:
 warnings.filterwarnings("ignore")
 url = "https://data.tycg.gov.tw/api/v1/rest/datastore/a1b4714b-3b75-4ff8-a8f2-cc377e4eaa0f?format=json"
 response = requests.get(url, verify=False)
-data = requests.get(url).json()
+data = response.json()
 df = pd.DataFrame(data["result"]["records"])
 
 try:
