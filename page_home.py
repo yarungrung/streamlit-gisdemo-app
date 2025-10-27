de4c935bf9aa7d50b5b4a5362c16e4b7eeaa5a59
yarungrung <s1243032@gm.ncue.edu.tw>
2025-10-27 05:58:00 +0000

    modified:   page_home.py

--- page_home.py
+++ page_home.py
@@ -2,14 +2,3 @@ import streamlit as st
 st.title("蔡亞蓉")
 st.header("蔡亞蓉的運用GIS功課")
 st.write("此app顯示所有桃園市youbike站點店為圖資")
-
- # 將MP4影片的URL傳給st.video() 
-video_url = "https://i.imgur.com/1GoAB0C.mp4"
- 
-st.write(f"正在播放影片：{video_url}")
-
-st.video(video_url)
-
- # 直接上傳照片的URL傳給st.image()
-image_url = "https://i.imgur.com/uf1T4ND.png"
-st.image(image_url)
